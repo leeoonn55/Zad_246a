a709635063b4557aa9faec0e2c20874ed6a0cbd8
Leszek Macura <leszek@macura.co>
2019-01-05 00:40:27 +0100

    Zadanie 24_6 Gotowe

---
+++ src/components/PlayersList/PlayerList.test.js
@@ -0,0 +1,43 @@
+import PlayersList from './PlayersList';
+import Player from '../Player/Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+  shallow(<PlayersList players={[]} />);
+});
+
+it('checks if number of players is correct', () => {
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5
+    },
+    {
+      name: 'Antoś',
+      score: 0
+    }
+  ]
+  const playerComponent = shallow(<PlayersList players={players} />);
+  const expectedPlayersNumber = playerComponent.find(Player).length;
+  expect(expectedPlayersNumber).toEqual(2);
+})
+
+it('checks if onScoreUpdate was called', () => {
+  const players = [
+    {
+      name: 'Kunegunda',
+      score: 5
+    },
+    {
+      name: 'Antoś',
+      score: 0
+    }
+  ]
+  const mockedOnScoreUpdate = jest.fn();
+  const playerComponent = shallow(<PlayersList players={players} onScoreUpdate={mockedOnScoreUpdate} />);
+  const firstPlayer = playerComponent.find(Player).first();
+  const onPlayerScoreChange = firstPlayer.prop('onPlayerScoreChange');
+  onPlayerScoreChange(10);
+  expect(mockedOnScoreUpdate).toBeCalledWith(0, 10);
+})
